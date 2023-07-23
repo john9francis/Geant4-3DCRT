3fc5e09dc2931fd75202c1b3639d8fd55b4f6216
John Francis <john9francis@gmail.com>
2023-07-22 21:37:38 -0600

    created basic cmake project

---
+++ Geant4-3DCRT.cc
@@ -0,0 +1,12 @@
+﻿// Geant4-3DCRT.cpp : Defines the entry point for the application.
+//
+
+#include "Geant4-3DCRT.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
